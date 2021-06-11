f82f7054bafbf62c653555f45befc2779eb29c74
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-06-11 08:49:54 +0200

    Création d'un composant dédié à la pagination

---
+++ front_pokedeck_manager/src/components/UI/Pagination.vue
@@ -0,0 +1,52 @@
+<template>
+   <section class="pagination">
+     <button><i class="fas fa-chevron-left"></i></button>
+     <button><i class="fas fa-chevron-right"></i></button>
+   </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Pagination',
+  data(){
+    return{
+    }
+  },
+  mounted() {
+  },
+  computed: {
+  },
+  methods: {
+  },
+}
+</script>
+
+
+<style scoped>
+
+
+.pagination{
+  width:100%;
+  display: flex;
+  justify-content: space-between;
+  margin:40px 0;
+}
+
+.pagination button{
+  width:60px;
+  height:60px;
+  border-radius: 50%;
+  border:none;
+  background-color: #e33441;
+  color:#FFF;
+  margin:0 40px;
+}
+
+.pagination button:hover{
+  background-color: #fa4753;
+  cursor: pointer;
+}
+
+</style>
