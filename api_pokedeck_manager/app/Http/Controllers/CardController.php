2c2eec892219c3953e3107cd356cfcee4df9b333
charly2221@hotmail.fr <charly2221@hotmail.fr>
2021-06-11 20:31:25 +0200

    Début de mise en place de l'API

---
+++ api_pokedeck_manager/app/Http/Controllers/CardController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CardResource;
+use App\Models\Card;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return CardResource
+     */
+    public function index($id_deck)
+    {
+        return new CardResource(Card::where('id_deck', $id_deck)->get());
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ api_pokedeck_manager/app/Http/Resources/CardResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CardResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+     return [
+         'id' => $this->id,
+         'id_deck' => $this->id_deck,
+         'card_name' => $this->card_name,
+         'card_quantity' => $this->card_quantity
+     ];
+    }
+}
